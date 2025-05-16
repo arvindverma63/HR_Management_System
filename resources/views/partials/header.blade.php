512dd0c70071530cdf1ab5a32a722ba4ce9a86e2
arvindverma63 <arvindverma630635@gmail.com>
2025-05-16 23:42:51 +0530

    update

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,14 +1,13 @@
   <!-- Header -->
-            <div
-                class="flex flex-col md:flex-row justify-between items-start md:items-center mb-6 md:mb-8 space-y-4 md:space-y-0">
-                <h2 class="text-2xl md:text-4xl font-bold text-gray-900 mr-10">Welcome, Admin</h2>
-                <div class="flex items-center space-x-4">
-                    <span class="text-sm md:text-lg text-gray-600">Friday, May 16, 2025</span>
-                    <div class="relative">
-                        <img src="https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_640.png" alt="User Avatar"
-                            class="w-8 md:w-10 h-8 md:h-10 rounded-full border-2 border-custom-blue">
-                        <span
-                            class="absolute bottom-0 right-0 w-2 md:w-3 h-2 md:h-3 bg-green-500 rounded-full border-2 border-white"></span>
-                    </div>
-                </div>
-            </div>
+  <div class="flex flex-col md:flex-row justify-between items-start md:items-center mb-6 md:mb-8 space-y-4 md:space-y-0">
+      <h2 class="text-2xl md:text-4xl font-bold text-gray-900 sm:ml-10" style="margin-left: 50px;">Welcome, Admin</h2>
+      <div class="flex items-center space-x-4">
+          <span class="text-sm md:text-lg text-gray-600">Friday, May 16, 2025</span>
+          <div class="relative">
+              <img src="https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_640.png" alt="User Avatar"
+                  class="w-8 md:w-10 h-8 md:h-10 rounded-full border-2 border-custom-blue">
+              <span
+                  class="absolute bottom-0 right-0 w-2 md:w-3 h-2 md:h-3 bg-green-500 rounded-full border-2 border-white"></span>
+          </div>
+      </div>
+  </div>
