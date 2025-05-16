b2c2bc2c2c0d0d02d01450fd679c3de1c8b61d2d
arvindverma63 <arvindverma630635@gmail.com>
2025-05-16 22:43:57 +0530

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    @vite('resources/css/app.css')
+     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
   </head>
   <body>
     <h1 class="text-3xl font-bold underline">
